a8eb639c48caa7ee5060df5c60399057bfb27a51
Chris Dziewa <chris.dziewa@gmail.com>
2016-03-14 16:43:10 -0500

    Created basic express server

---
+++ server.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var express = require('express');
+var app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello world');
+});
+
+app.listen(3000, () => {
+  console.log('app listening on port 3000');
+})
