a3c81dcba5401c544c70abd785c19a9168ea9af3
Chris Dziewa <chris.dziewa@gmail.com>
2016-03-16 17:03:18 -0500

    Add sessions to app

--- app/config/index.js
+++ app/config/index.js
@@ -4,7 +4,8 @@ if (process.env.NODE_ENV === 'production') {
   // Offer production stage env vars
   module.exports = {
     host: process.env.host || "",
-    dbURI: process.env.dbURI
+    dbURI: process.env.dbURI,
+    "sessionSecret": process.env.sessionSecret
   }
 } else {
   module.exports = require('./development.json');
---
+++ app/session/index.js
@@ -0,0 +1,24 @@
+'use strict';
+const session = require('session');
+const MongoStore = require('connect-mongo')(session);
+const config = require('../config');
+const db = require('../db');
+
+if (process.env.NODE_ENV === 'production') {
+  // Initialize session with production settings
+  module.exports = session({
+    secret: config.sessionSecret,
+    resave: false,
+    saveUninitialized: false,
+    store: new MongoStore({
+      mongooseConnection: db.mongoose.connection
+    }) // Where sessions are stored
+  });
+} else {
+  // Initialize session with development settings
+  module.exports = session({
+    secret: config.sessionSecret,
+    resave: false, // prevent calls to DB when session has not changed
+    saveUninitialized: true // stores session even when no data is available to be stored
+  });
+}
