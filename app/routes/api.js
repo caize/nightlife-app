162afc6624929d0532129e9606592189f89741c9
Chris Dziewa <chris.dziewa@gmail.com>
2016-04-07 15:13:59 -0500

    Comment out code for combining db data and yelp response

--- app/routes/api.js
+++ app/routes/api.js
@@ -21,15 +21,15 @@ apiRouter.get('/yelp/:location', (req, res, next) => {
     }
     else {
       let apiResults = JSON.parse(body);
-      // Get all db venue entries
-      h.getAllVenues().then(dbVenues => {
-        console.log(dbVenues);
-      })
-      .catch(e => {
-        res.send(e);
-      })
-      // iterate through apiResults and check if it is in db
-      // If in DB, add user list to venue for response
+      // // Get all db venue entries
+      // h.getAllVenues().then(dbVenues => {
+      //   console.log(dbVenues);
+      // })
+      // .catch(e => {
+      //   console.log('Error: ', e);
+      // })
+      // // iterate through apiResults and check if it is in db
+      // // If in DB, add user list to venue for response
 
       return res.json(apiResults);
     }
